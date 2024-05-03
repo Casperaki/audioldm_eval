0b27819d1d4cc53e4c76d64aa636e42349eeac6c
Harshita <seth.harshi@gmail.com>
2024-05-03 17:05:19 +0100

    update

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import torch
 from audioldm_eval import EvaluationHelper
-
+import sys
 device = torch.device(f"cuda:{0}")
 
-generation_result_path = "example/paired"
+
+generation_result_path = sys.argv[1]
 # generation_result_path = "example/unpaired"
-target_audio_path = "example/reference"
+target_audio_path = sys.argv[2]
 
 evaluator = EvaluationHelper(16000, device)
 
